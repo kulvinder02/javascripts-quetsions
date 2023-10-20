77b0676cdfc3eeab1726774832ae87ec7b38b740
kulvinder02 <kulvinderjangra02@gmail.com>
2023-10-20 09:33:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+//  1  ---------> sum off all elemnt in array 
+// const a = [2, 3, 4, 5, 5, 5, 5]
+// var sum = 0;
+// for(let i=0; i < a.length; i++){
+//     sum += a[i];
+//     console.log(sum)
+// }
+
+// 2 ------------ rverse of a string
+// let str = "ajskals"; 
+// let revsestr = str.split('').reverse().join(''); 
+// console.log(revsestr);
+
+// 3 -------------even number check
+//  let i = 11
+//       if (i % 2 == 0){
+//         console.log(true);
+//       }
+//       else{
+//         console.log(false)
+//       }
+
+// 4---------------maximum elemnt in array 
+//      let i =[122,12,21,21,21];
+//      let b =i.map( (e)=> e);
+//      console.log(b.length)
+
+
+// 5 --------------FActorial 
+// function Factorialnum(k) {
+//     let factorial = 1;
+//     for (let i = k; i >= 1; i--) {
+//         factorial *= i;
+//     }
+//     console.log(factorial);
+// };
+// Factorialnum();
+
+
+// 7----------- remove duplicate element 
+//    let arr = ["apple", "mango", "apple", 
+//           "orange", "mango", "mango"]; 
+     
+//   function removeDuplicates(arr) { 
+//     return [...new Set(arr)]; 
+//    } 
+//    console.log(removeDuplicates(arr));
+
+// 8-----------second max-number
+// let a = [116,245,92,333,123];
+// let maxnum = -Infinity;
+// let secondMaxnum = -Infinity;
+// for (var i = 0; i < a.length; i++) {
+//     maxnum = Math.max(maxnum, a[i])
+// }
+// for (let i = 0; i < a.length; i++) {
+//     if (a[i] < maxnum) {
+//         secondMaxnum = Math.max(secondMaxnum, a[i])
+//     }
+// }
+// console.log(maxnum + '  => First number');
+// console.log(secondMaxnum  + '  => second number');
+
+
+//9 -------------prime number check
+// let i = 0;
+
+// if (i > 1 && i % 2 == 0) {
+//     console.log(true);
+// } else {
+//     console.log(false)
+// }
+
+// 10 ------------ rverse of a numnber
+// let num = 123456789; 
+// let revsenum = num.toString().split('').reverse().join(''); 
+// console.log(revsenum);
+
+
+    
